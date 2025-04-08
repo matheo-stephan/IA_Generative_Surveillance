2a3817cdb056ace03eeff83d95f7dc5da9fa4eb5
Thibault MEGARD <158142347+Thibault-megard1@users.noreply.github.com>
2025-04-08 14:56:33 +0200

    Add files via upload

---
+++ embedding.py
@@ -0,0 +1,25 @@
+import torch
+import clip
+from PIL import Image
+import requests
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+import numpy as np
+from scipy.spatial.distance import cosine, euclidean
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+def extract_embedding(frames):
+    image_embedding = model.encode_image(frames).float()
+    return image_embedding.cpu().numpy()
+
+def compute_similarity(embedding1, embedding2):
+    """
+    Compute cosine similarity between image and text embeddings.
+    """
+    similarity_cos = 1 - cosine(embedding1.flatten(), embedding2.flatten())
+    return similarity_cos
